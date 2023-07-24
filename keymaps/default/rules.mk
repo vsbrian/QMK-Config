e46fa9bf5df7dd8aff6d0c034faa9ee23df93299
vsbrian <127261808+vsbrian@users.noreply.github.com>
2023-07-24 11:32:03 -0500

    Update rules.mk

--- keymaps/default/rules.mk
+++ keymaps/default/rules.mk
@@ -1,7 +1,7 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+COMBO_ENABLE = yes
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 SRC += features/layer_lock.c
-COMBO_ENABLE = yes
