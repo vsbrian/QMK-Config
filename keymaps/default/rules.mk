3109a0c6c83c71751ac78c6df0c5288cd954f242
vsbrian <127261808+vsbrian@users.noreply.github.com>
2023-07-28 14:50:29 -0500

    Update rules.mk

--- keymaps/default/rules.mk
+++ keymaps/default/rules.mk
@@ -4,4 +4,3 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 COMBO_ENABLE = yes
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-SRC += features/layer_lock.c
