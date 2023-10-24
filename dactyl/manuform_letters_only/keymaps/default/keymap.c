8ae2b9b869704034ddd3d3de4d6ceea6a1519e27
vsbrian <127261808+vsbrian@users.noreply.github.com>
2023-10-24 13:26:53 -0500

    Update keymap.c

--- dactyl/manuform_letters_only/keymaps/default/keymap.c
+++ dactyl/manuform_letters_only/keymaps/default/keymap.c
@@ -65,6 +65,14 @@ void matrix_scan_user(void) {
   achordion_task();
 }
 
+const key_override_t EM_key_override = ko_make_basic(MOD_MASK_SHIFT, EM, PW);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&EM_key_override,
+	NULL // Null terminate the array of overrides!
+};
+
 // Aliases for readability
 #define QWERTY   DF(_QWERTY)
 #define COLEMAK  DF(_COLEMAK_DH)
